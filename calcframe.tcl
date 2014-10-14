96fbc8e1e3ec97857553dd454979b9fa9b263cb8
Ivan Sukin <isukin.intelliware@gmail.com>
2014-10-14 14:44:18 +0400

    too much additions

--- calcframe.tcl
+++ calcframe.tcl
@@ -17,8 +17,10 @@ proc rowtag row {
     if {$row == 0} {
 	return title
     } elseif {$row == 1 || $row == 7 || \
-	$row == 9 || $row == 12 || $row == 15} {
+		  $row == 9 || $row == 12 || $row == 15} {
 	return subtitle
+    } elseif {$row == 17} {
+	return results
     }
 }
 
@@ -37,24 +39,83 @@ set ftvals(7,0) "Высококипящий компонент"
 set ftvals(9,0) "Параметры куба колонны"
 set ftvals(12,0) "Параметры дефлегматора колонны"
 set ftvals(15,0) "Параметры массопереноса"
+set ftvals(17,0) "Результаты"
+
+set ftvals(2,2) "моль/моль"
+set ftvals(3,2) "моль/моль"
+set ftvals(4,2) "моль/моль"
+set ftvals(5,2) "К"
+set ftvals(6,2) "кДж/моль"
+
+set ftvals(8,2) "К"
+
+set ftvals(10,2) "К"
+set ftvals(11,2) "Вт/К"
 
 set ftable $fcol1.table
+set fvoc1 $ftable.voc1
+set fvoc2 $ftable.voc2
 table $ftable -variable ftvals \
     -colwidth 15 -cols 4 \
-    -rows 17 \
+    -rows 20 \
     -rowtagcommand rowtag -coltagcommand coltag \
-    -selectmode none
+    -selectmode browse \
+    -cursor arrow \
+    -resizeborders none
 pack $ftable
+
+ttk::button $fvoc1 -text "Открыть" -cursor arrow
+ttk::button $fvoc2 -text "Открыть" -cursor arrow
+$ftable window configure 2,3 -window $fvoc1 -sticky nwse
+$ftable window configure 8,3 -window $fvoc2 -sticky nwse
+$ftable height 8 2
+
+ttk::entry $ftable.e1
+ttk::entry $ftable.e2
+ttk::entry $ftable.e3
+ttk::entry $ftable.e4
+ttk::entry $ftable.e5
+ttk::entry $ftable.e6
+ttk::entry $ftable.e7
+ttk::entry $ftable.e8
+ttk::entry $ftable.e9
+ttk::entry $ftable.e10
+ttk::entry $ftable.e11
+ttk::entry $ftable.e12 -state readonly
+ttk::entry $ftable.e13 -state readonly
+$ftable window configure 2,1 -window $ftable.e1 -sticky nwse
+$ftable window configure 3,1 -window $ftable.e2 -sticky nwse
+$ftable window configure 4,1 -window $ftable.e3 -sticky nwse
+$ftable window configure 5,1 -window $ftable.e4 -sticky nwse
+$ftable window configure 6,1 -window $ftable.e5 -sticky nwse
+$ftable window configure 8,1 -window $ftable.e6 -sticky nwse
+$ftable window configure 10,1 -window $ftable.e7 -sticky nwse
+$ftable window configure 11,1 -window $ftable.e8 -sticky nwse
+$ftable window configure 13,1 -window $ftable.e9 -sticky nwse
+$ftable window configure 14,1 -window $ftable.e10 -sticky nwse
+$ftable window configure 16,1 -window $ftable.e11 -sticky nwse
+$ftable window configure 18,1 -window $ftable.e12 -sticky nwse
+$ftable window configure 19,1 -window $ftable.e13 -sticky nwse
+for {set i 1} {$i < 14} {incr i} {
+    $ftable.e$i configure -justify right
+}
 
 $ftable spans 1,0 0,4
 $ftable spans 7,0 0,4
 $ftable spans 9,0 0,4
 $ftable spans 12,0 0,4
 $ftable spans 15,0 0,4
+$ftable spans 17,0 0,4
 
+$ftable spans 2,3  4,0
+
+$ftable tag col dim 2
+
+$ftable tag configure dim -bg lightblue -state disabled
 $ftable tag configure readonly -state disabled
 $ftable tag configure title -bg lightgreen -fg black
 $ftable tag configure subtitle -bg yellow -fg black
+$ftable tag configure results -bg salmon -fg black
 
 
 set fcanvas $fcol2.canvas
